88200bd97c4e3634e497ce35d46b04da3396a26b
makerbro <makerbro@acrobotic.com>
2020-03-28 14:15:40 -0700

    updating iss-tracker demo

---
+++ iss-tracker/animate.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import colorsys
+import time
+from sys import exit
+
+try:
+    import numpy as np
+except ImportError:
+    exit('This script requires the numpy module\nInstall with: sudo pip install numpy')
+
+import blinkt
+
+blinkt.set_clear_on_exit()
+
+def make_gaussian(fwhm):
+    x = np.arange(0, blinkt.NUM_PIXELS, 1, float)
+    y = x[:, np.newaxis]
+    x0, y0 = 3.5, 3.5
+    fwhm = fwhm
+    gauss = np.exp(-4 * np.log(2) * ((x - x0) ** 2 + (y - y0) ** 2) / fwhm ** 2)
+    return gauss
+
+def animate(hue, times=1):
+    for i in range(times):
+        for z in list(range(1, 10)[::-1]) + list(range(1, 10)):
+            fwhm = 5.0 / z
+            gauss = make_gaussian(fwhm)
+            start = time.time()
+            y = 4
+
+            for x in range(blinkt.NUM_PIXELS):
+                h = hue
+                s = 1.0
+                v = gauss[x, y]
+                rgb = colorsys.hsv_to_rgb(h, s, v)
+                r, g, b = [int(255.0 * i) for i in rgb]
+                blinkt.set_pixel(x, r, g, b)
+
+            blinkt.show()
+            end = time.time()
+            t = end - start
+
+            if t < 0.04:
+                time.sleep(0.04 - t)
+
+if(__name__ == '__main__'):
+    animate(0.9,2)
--- iss-tracker/iss_tracker.py
+++ iss-tracker/iss_tracker.py
@@ -1,7 +1,8 @@
 #!/bin/python3
 from flask import Flask, render_template, request
 import json
-import sys
+from animate import animate
+
 filename = "/boot/api_key.txt"
 api_key = ''
 with open(filename,'r') as f:
@@ -25,17 +26,20 @@ def blink():
     status = {}
     req = request.get_json()
     if(req["next_pass"] is not None):
-        animate(req["next_pass"])
+        show_animation(req["next_pass"])
         status["status"] = "OK"
     else:
         status["status"] = "NOT OK"
     return json.dumps(status)
 
-def animate(time):
+def show_animation(time):
     if(time>60):
+        animate(275./360,2)
         return
     if(time>1):
+        animate(200./360,5)
         return
+    animate(30./360,8)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
