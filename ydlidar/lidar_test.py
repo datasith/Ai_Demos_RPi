7f731b74d5fd653965f17299da2d8d84acb29933
Andreas Dohren <dohren@web.de>
2021-08-29 12:28:56 +0200

    Python3 for Test

--- ydlidar/lidar_test.py
+++ ydlidar/lidar_test.py
@@ -1,17 +1,19 @@
-import PyLidar2
+import PyLidar3
 import time # Time module
 #Serial port to which lidar connected, Get it from device manager windows
 #In linux type in terminal -- ls /dev/tty* 
 port = "/dev/ttyUSB0" #linux
-Obj = PyLidar2.YdLidarX4(port)
+Obj = PyLidar3.YdLidarX4(port)
 if(Obj.Connect()):
     print(Obj.GetDeviceInfo())
     gen = Obj.StartScanning()
     t = time.time() # start time 
     while (time.time() - t) < 30: #scan for 30 seconds
-        print(gen.next())
+        #print(gen.next())
+        print(next(gen))
         time.sleep(0.5)
     Obj.StopScanning()
     Obj.Disconnect()
 else:
     print("Error connecting to device")
+
